341f29bcffbf2a40d633e10810550bfbd5b4439a
jjin43 <jjin43@asu.edu>
2023-10-29 02:05:23 -0700

    ALL functionalities finalized, combined Kaly's commit.

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -6,45 +6,42 @@ function App() {
   return (
     <>
       <h1>Health Insurance Predictor</h1>
-      <body>
         <div className="card">
           <form action="http://localhost:5000/submit" method='POST' id='form1'>
             <label htmlFor='age'>Age:</label>
-            <input type='number' name='age' id='ageX' />
+            <input type='number' name='age' id='ageX' required/>
 
             <label htmlFor='sex'>Sex:</label>
-            <select name="sex" id='sexX' >
+            <select name="sex" id='sexX' required>
               <option value="1"> Male </option>
               <option value="0"> Female </option>
             </select>
 
             <label htmlFor='bmi'>BMI:</label>
-            <input type='number' name='bmi' id='bmiX' />
+            <input type='number' name='bmi' id='bmiX' required />
 
             <label htmlFor='children'>Number of Children:</label>
-            <input type='number' name='children' id='childrenX'/>
+            <input type='number' name='children' id='childrenX' required/>
 
             <label htmlFor='smoker'>Smoker:</label>
-            <select name='smoker' id='smokerX'>
+            <select name='smoker' id='smokerX' required>
               <option value="0"> No </option>
               <option value="1"> Yes </option>
             </select>
 
             <label htmlFor='region'>Region:</label>
-            <select name='region' id='regionX'>
+            <select name='region' id='regionX' required>
               <option value="1">  northwest  </option>
               <option value="2"> northeast </option>
               <option value="3"> southwest </option>
               <option value ="4"> southeast </option>
             </select>
             <div>
-              <input type='submit' id='calcButton' value="Calculate" disabled form='form1'/>
+              <input type='submit' id='calcButton' value="Calculate" form='form1'/>
             </div>
           </form>
         
         </div>
-      </body>
-
     </>
   )
 }
